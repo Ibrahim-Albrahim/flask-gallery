00a3b754614b75151795f11ea346897e663f3544
Ibrahim-Albrahim <86062781+Ibrahim-Albrahim@users.noreply.github.com>
2021-09-24 22:58:21 +0300

    complete component's job which is adding new photo using flask api

---
+++ frontend/src/AddPhoto.js
@@ -0,0 +1,94 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Modal, Image, Form} from 'react-bootstrap';
+import React, { Component, useState } from 'react'
+
+const apiUrl = 'http://localhost:5000/'
+
+
+class AddPhoto extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      open : props.open,
+      galleryId : null,
+      file : null,
+    }
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.fileSelectHandler = this.fileSelectHandler.bind(this);
+    this.galleryIdChangeHandler = this.galleryIdChangeHandler.bind(this);
+  }
+
+  fileSelectHandler = (event) => {
+    this.setState({file : event.target.files[0]})
+  }
+
+  galleryIdChangeHandler = (event) => {
+    this.setState({galleryId : event.target.value})
+  }
+
+
+  
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    let file = this.state.file;
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("galleryId", this.state.galleryId);
+
+
+
+    fetch(apiUrl+'photo/create/json' , {
+      method: 'POST',
+      body: formData
+    })
+    .then(() => {
+      document.getElementById("add-photo-form").reset();
+      return;
+    }).then(
+      success => console.log(success) // Handle the success response object
+    ).catch(
+      error => console.log(error) // Handle the error response object
+    )
+  };
+
+    
+    render(){
+      return (
+        <>
+          <Modal
+            className="add-gallery-modal"
+            show= {this.state.open}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header >
+              <Modal.Title>New Photo</Modal.Title>
+              {this.props.children}
+            </Modal.Header>
+
+            <Modal.Body className="add-gallery-form">
+              <Form onSubmit={this.onFormSubmit} id="add-photo-form"  enctype="multipart/form-data">
+                <Form.Group controlId="formFile" className="mb-3">
+                  <div className="title-div" >
+                    <Form.Label>Gallery Id:</Form.Label>
+                    <Form.Control className="gallery-title" type="number" placeholder="0" onChange={this.galleryIdChangeHandler} required />
+                  </div>
+                  <div className="file-div">
+                    <Form.Label>photo:</Form.Label>
+                    <Form.Control type="file" className="gallery-thum" onChange={this.fileSelectHandler} required/>
+                  </div>
+                  <Button variant="primary" type="submit">Add</Button>
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            
+            <Modal.Footer className="footer">
+            </Modal.Footer>
+          </Modal>
+        </>
+      );
+    }
+
+}
+export default AddPhoto
