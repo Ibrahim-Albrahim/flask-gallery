fcfe353833d616966c2ad4f538aea60c7353ecc9
Ibrahim-Albrahim <86062781+Ibrahim-Albrahim@users.noreply.github.com>
2021-10-21 01:25:28 +0300

    fetch is inside function and recall it on every update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,29 +10,38 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      apiUrl: 'http://localhost:5000/',
+      apiUrl: 'http://192.168.43.41:5000/',
       galleries: [],
       isLoaded: false,
-      showAddGalleryModal: false,
-      showAddPhotoModal: false,
     }
   }
 
   componentDidMount(){
-    fetch(this.state.apiUrl+'json')
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          galleries: json,
-        })
-      });
+    // this.getData()
   }
-  
+
+  getData () {
+    fetch(this.state.apiUrl)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        galleries: json,
+      })
+    });
+  }
+
+  makeIsLoadedFalse () {
+    this.setState({
+      isLoaded: false,
+    })
+  }
 
   render(){
     var {isLoaded , galleries} = this.state;
-    // console.log(isLoaded,galleries)
+
+    !this.state.isLoaded ? this.getData() :null
+
 
     return (
       <div className="app-header banner-two">
@@ -41,28 +50,16 @@ class App extends Component {
             MyGallery
           </h1>
           <div>
-              <a className="btn cold" onClick={() => this.setState({showAddGalleryModal: true})}> Add Gallery </a>
-              <a className="btn cold" onClick={() => this.setState({showAddPhotoModal: true})}> Add Photo </a>
+              <AddGallery apiUrl={this.state.apiUrl}  makeIsLoadedFalse={this.makeIsLoadedFalse.bind(this)} />
+              <AddPhoto apiUrl={this.state.apiUrl}  makeIsLoadedFalse={this.makeIsLoadedFalse.bind(this)} />
           </div>
         </header>
 
         <div className="images-container" >
           {galleries.map(gallery => (
-            <Gallery apiUrl={this.state.apiUrl} key={gallery.id} id={gallery.id} pic= {gallery.img} title = {gallery.title}/>
+            <Gallery apiUrl={this.state.apiUrl} key={gallery.id} id={gallery.id} pic= {gallery.img} title = {gallery.title} makeIsLoadedFalse={this.makeIsLoadedFalse.bind(this)}/>
           ))}
         </div>
-
-        {this.state.showAddGalleryModal && 
-          <AddGallery apiUrl={this.state.apiUrl} open>
-            <Button variant="danger" className="btn-close" onClick={() => this.setState({showAddGalleryModal: false})}/>
-          </AddGallery>
-        }
-
-        {this.state.showAddPhotoModal && 
-          <AddPhoto apiUrl={this.state.apiUrl} open>
-            <Button variant="danger" className="btn-close" onClick={() => this.setState({showAddPhotoModal: false})}/>
-          </AddPhoto>
-        }
       </div>
     );
   }
