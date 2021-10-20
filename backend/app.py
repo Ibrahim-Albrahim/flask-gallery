d59ee6bdcf8cf397fca93327d9090d0a35c70e97
Ibrahim-Albrahim <86062781+Ibrahim-Albrahim@users.noreply.github.com>
2021-10-21 01:21:18 +0300

    remove all functions that runs flask templates
    remove all functions that runs flask templates and use only api functions

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,8 @@ from models import Photo,Gallery, setup_db, db
 from flask_moment import Moment
 import base64
 
+
+
 app = Flask(__name__)
 moment = Moment(app)
 
@@ -22,13 +24,20 @@ app.secret_key = os.urandom(32)
 CORS(app)
 
 def render_picture(data):
-
     render_pic = base64.b64encode(data).decode('ascii') 
     return render_pic
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', galleries=Gallery.query.all())
+    galleries=Gallery.query.all()
+    data = []
+    for gallery in galleries:
+        data.append({
+            'id': gallery.id,
+            'title' : gallery.title,
+            'img' : gallery.rendered_data
+        })
+    return jsonify(data)
 
 @app.route('/<gallery_id>' , methods=['GET'])
 def show_gallery(gallery_id):
@@ -38,39 +47,33 @@ def show_gallery(gallery_id):
     gallery = Gallery.query.get(photo.gallery_id)
     data.append ({
         'id': photo.id,
-        'rendered_data': photo.rendered_data,
+        'file_name' : photo.name,
+        'img': photo.rendered_data,
     })
 
-  return render_template('gallery.html',photosdata=data)
-    
-@app.route('/add-gallery')
-def addGallery():
-  return render_template('add-gallery.html')
-
+  return jsonify(data)
 
 @app.route('/gallery/create', methods=['POST'])
 def create_gallery():
-   file = request.files['inputFile']
+   file = request.files['file']
    data = file.read()
    render_file = render_picture(data)
    title = request.form['title']
 
-   newFile = Gallery(name=file.filename, data=data, rendered_data=render_file, title=title)
    try:
+    newFile = Gallery(name=file.filename, data=data, rendered_data=render_file, title=title)
     db.session.add(newFile)
     db.session.commit() 
-   except:
+    result = {"success": True}
+    return jsonify(result)
+
+   except ImportError:
     db.session.rollback()
-    flash(f'An error occurred creating gallery {title}.')
+    result = {"success": False}
+    return jsonify(result)
+    
    finally:
        db.session.close() 
-   return redirect(url_for('index'))
-        
-@app.route('/<gallery_id>/edit', methods=['PATCH','GET'])
-def edit_gallery(gallery_id):
-    gallery = Gallery.query.get(gallery_id)
-    return render_template('edit-gallery.html', gallery=gallery)
-
 
 @app.route('/<gallery_id>/edit', methods=['PATCH','POST'])
 def edit_gallery_submission(gallery_id):
@@ -119,16 +122,12 @@ def delete_gallery(gallery_id):
         else: return redirect(url_for('index')) 
 
 
-@app.route('/add-photo')
-def addPhoto():
-  return render_template('add-photo.html')
-
 @app.route('/photo/create', methods=['POST'])
 def create_photo():
-   file = request.files['inputFile']
+   file = request.files['file']
    data = file.read()
    render_file = render_picture(data)
-   gallery_id = int(request.form['gallery'])
+   gallery_id = int(request.form['galleryId'])
 
    newFile = Photo(name=file.filename, data=data, rendered_data=render_file, gallery_id=gallery_id)
 
@@ -142,9 +141,6 @@ def create_photo():
     flash(f'An error occurred adding photo.')
    finally:
        db.session.close() 
-
-
-   return redirect(url_for('index')) 
 
 
 @app.route('/photo/<photo_id>/delete', methods=['DELETE','GET'])
@@ -246,4 +242,4 @@ def create_photo_json():
    return redirect(url_for('index')) 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='192.168.43.41', port=5000, debug=True)
