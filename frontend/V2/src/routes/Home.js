8e0c243c54ad9b0fc9c192852f241d6890ef3730
Ibrahim Albrahim <hemo7f12@gmail.com>
2021-12-25 03:34:16 +0300

    replace 'a' tag with 'Link' and convert to function

--- frontend/V2/src/routes/Home.js
+++ frontend/V2/src/routes/Home.js
@@ -1,82 +1,64 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactLoading from 'react-loading';
 import '../scss/Home.scss';
 import { Image } from 'react-bootstrap';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
-import Footer from './components/Footer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import Footer from './components/Footer';
+import { apiUrl } from '../config';
+import { Link } from 'react-router-dom';
 
-class Home extends Component {
+const Home = () => {
+  const [galleries , setGalleries] = useState({galleries:[], isLoaded : false});
+  const settings = {
+    className:'slider',
+    dots: true,
+    infinite: false,
+    speed: 500,
+    // slidesToShow: 1,
+    autoplay: false,
+    focusOnSelect: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    rows: 1,
+    swipeToSlide: true,
+    variableWidth: true,
+  };
 
-  constructor(props){
-    super(props);
-    this.state = {
-      // apiUrl: 'http://192.168.43.41:5000/',
-      apiUrl: 'http://localhost:5000/',
-      galleries: [],
-      isLoaded: false,
-    }
-  }
-
-  getData () {
-    fetch(this.state.apiUrl)
-    .then(response => response.json())
-    .then(json => {
-      this.setState({
-        isLoaded: true,
-        galleries: json,
-      })
-    });
-  }
-
-  makeIsLoadedFalse () {
-    this.setState({
-      isLoaded: false,
-    })
-  }
-
-  render(){
-    var {isLoaded , galleries} = this.state;
-    if (!isLoaded) { this.getData()} 
-    const settings = {
-      className:'slider',
-      dots: true,
-      infinite: false,
-      speed: 500,
-      // slidesToShow: 1,
-      autoplay: false,
-      focusOnSelect: true,
-      pauseOnFocus: true,
-      pauseOnHover: true,
-      rows: 1,
-      swipeToSlide: true,
-      variableWidth: true,
+  useEffect(() => {
+    const fetchData = async () => {
+        await fetch(apiUrl)
+            .then(response => response.json())
+            .then(json => setGalleries({galleries: json , isLoaded : true}) );
     };
+
+    fetchData();
+  },[]);
+   
+  return (
+
+    galleries.isLoaded ? <div className="home-container">
+          <Slider {...settings}>
+            {galleries.galleries.map(gallery => (
+              <Link to={'gallery/'+gallery.id} >
+                <div className='view-gallery-photo'>
+                <Image src={"data:;base64,"+gallery.img}/>
+                <div className="gallery-info">
+                <FontAwesomeIcon className='faEye' icon={faEye} />
+                  <h3> {gallery.id} | {gallery.title} </h3>
+                </div>
+              </div></Link>
+            ))}
+          </Slider>
+          <Footer />
+      </div>
+    : <ReactLoading type="spinningBubbles" color="#ffffff" height={300} width={300} className="spinning-bubbles"/>
     
-    return (
-
-      !isLoaded ? <ReactLoading type="spinningBubbles" color="#ffffff" height={300} width={300} className="spinning-bubbles"/>
-      : <div className="home-container">
-            <Slider {...settings}>
-              {galleries.map(gallery => (
-                <a href={'/gallery/'+gallery.id} ><div className='view-gallery-photo'>
-                  <Image src={"data:;base64,"+gallery.img}/>
-                  <div className="gallery-info">
-                  <FontAwesomeIcon className='faEye' icon={faEye} />
-                    <h3> {gallery.id} | {gallery.title} </h3>
-                  </div>
-                </div></a>
-              ))}
-            </Slider>
-            <Footer />
-        </div>
-      
-
-    );
-  }
+  );
 }
+
 
 export default Home;
