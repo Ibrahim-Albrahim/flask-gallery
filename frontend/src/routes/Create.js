33e751244720b34512856ab7b516363e36edb645
Ibrahim Albrahim <hemo7f12@gmail.com>
2022-01-23 02:54:50 +0300

    fix style and minimize code

--- frontend/src/routes/Create.js
+++ frontend/src/routes/Create.js
@@ -7,62 +7,62 @@ import { Button, Form } from 'react-bootstrap';
 
 const Home = () => {
     const apiUrl = 'http://localhost:5000/'
-    const [isUploading , setIsUploading] = useState({isUploading: false})
-    const [galleryForm] = useState(new FormData())
-    const [photoForm] = useState(new FormData())
+    const [isUploading , setIsUploading] = useState({gallery: false , photo: false})
+    const [galleryForm , setGalleryForm] = useState(new FormData())
+    const [photoForm , setPhotoForm] = useState(new FormData())
 
     async function onGalleryFormSubmit (event) {
         event.preventDefault();
-        setIsUploading({isUploading: true})
+        setIsUploading({gallery: true})
         galleryForm.append('password' , document.getElementById('gallery-password').value)
         galleryForm.append('title' , document.getElementById('gallery-title').value)
         galleryForm.append('file' , document.getElementById('gallery-file').files[0])
         await fetch(apiUrl+'gallery/create' , {method: 'POST', body: galleryForm})
             .then(() => {
+                setGalleryForm(new FormData())
                 document.getElementById("add-gallery-form").reset();
-                setIsUploading({isUploading: false})
+                setIsUploading({gallery: false})
                 return;
             })
     };
 
     async function onPhotoFormSubmit (event) {
         event.preventDefault();
-        setIsUploading({isUploading: true})
+        setIsUploading({photo: true})
         photoForm.append('password' , document.getElementById('photo-password').value)
         photoForm.append('galleryId' , document.getElementById('photo-gallery-id').value)
         var totalfiles = document.getElementById('photo-file').files.length;
         for (var index=0; index < totalfiles; index++){ photoForm.append('file' , document.getElementById('photo-file').files[index]) }
         await fetch(apiUrl+'photo/create' , {method: 'POST', body: photoForm})
             .then(() => {
+                setPhotoForm(new FormData())
                 document.getElementById("add-photo-form").reset();
-                setIsUploading({isUploading: false})
+                setIsUploading({photo: false})
                 return;
             })
     };
 
+    const loading = <ReactLoading type="spinningBubbles" color="#ffffff" height={200} width={200} className="loading-bubbles"/>;
+
     return (
         <div className="create-container">
             <section className='add-gallery-section'>
+                {isUploading.gallery? loading : null}
                 <h1>Add New Gallery</h1>
                 <Form id="add-gallery-form" onSubmit={onGalleryFormSubmit.bind(this)} encType="multipart/form-data">
-                    <Form.Label >Gallery title:</Form.Label>
-                    <Form.Control id="gallery-title" type="text" placeholder="title" required />
-                    <Form.Label >Gallery photo:</Form.Label>
-                    <Form.Control type="file" id="gallery-file" name="inputFile" accept="image/*" required/>
-                    <Form.Label>password:</Form.Label>
-                    <Form.Control type="password" id="gallery-password" required />
+                    <Form.Label className='lbl'> <span>Title:</span>    <Form.Control id="gallery-title" type="text" placeholder="title" required /> </Form.Label>
+                    <Form.Label className='lbl'> <span>Thumnail:</span> <Form.Control id="gallery-file" type="file" accept="image/*" required />     </Form.Label>
+                    <Form.Label className='lbl'> <span>Password:</span> <Form.Control id="gallery-password" type="password" required />              </Form.Label>
                     <Button type="submit">Add Gallery</Button>
                 </Form>
             </section>
             <section className='add-photo-section'>
+                {isUploading.photo? loading : null}
                 <h1>Add New Photo</h1>
                 <Form id="add-photo-form" onSubmit={onPhotoFormSubmit.bind(this)} encType="multipart/form-data">
-                    <Form.Label>Gallery Id:</Form.Label>
-                    <Form.Control id="photo-gallery-id" type="number" placeholder="0" required />
-                    <Form.Label>photo:</Form.Label>
-                    <Form.Control id="photo-file" type="file" accept="image/*" required multiple/>
-                    <Form.Label>password:</Form.Label>
-                    <Form.Control id="photo-password" type="password" required />
+                    <Form.Label className='lbl'> <span>Gallery ID:</span> <Form.Control id="photo-gallery-id" type="number" placeholder="0" required />   </Form.Label>
+                    <Form.Label className="lbl"> <span>Photo:</span>      <Form.Control id="photo-file" type="file" accept="image/*" required multiple /> </Form.Label>
+                    <Form.Label className='lbl'> <span>Password:</span>   <Form.Control id="photo-password" type="password" required />                   </Form.Label>
                     <Button type="submit">Add Photo</Button>
                 </Form>
             </section>
@@ -70,6 +70,4 @@ const Home = () => {
         </div>
     );
 }
-
-
 export default Home;
