477e42cf8114c2fb0b9def30f1552818f8265656
Ibrahim Albrahim <hemo7f12@gmail.com>
2022-02-17 00:01:53 +0300

    add Slider component

---
+++ frontend/src/routes/components/Sliders.js
@@ -0,0 +1,42 @@
+import Slider from "react-slick";
+import { Image } from 'react-bootstrap';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import '../../scss/Sliders.scss';
+
+const Sliders = (props) => {
+    const settings = {
+        className:'slider',
+        dots: true,
+        infinite: false,
+        speed: 500,
+        // slidesToShow: 1,
+        autoplay: false,
+        focusOnSelect: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        rows: 1,
+        swipeToSlide: true,
+        variableWidth: true,
+    };
+    
+    return  (    
+        <Slider {...settings}>
+            {props.imgs.map(img => (
+            <Link key={img.id} to={props.link+img.id} >
+                <div className='view-photo'>
+                    <Image src={"data:;base64,"+img.small_size }/>
+                    <div className="info">
+                        <FontAwesomeIcon className='fa-eye' icon={faEye} />
+                        <h3> {img.id} {props.splitter} {img.title} </h3>
+                    </div>
+                </div>
+            </Link>
+            ))}
+        </Slider>
+    )
+};
+export default Sliders;
