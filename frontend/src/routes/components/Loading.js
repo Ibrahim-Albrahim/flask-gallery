9cd586f9a7010ee738e4f58d3a4a033bdfa102d0
Ibrahim Albrahim <hemo7f12@gmail.com>
2022-02-17 00:02:14 +0300

    add Loading component

---
+++ frontend/src/routes/components/Loading.js
@@ -0,0 +1,6 @@
+import ReactLoading from 'react-loading';
+
+const Loading = () => {
+    return  <ReactLoading type="spinningBubbles" color="#ffffff" height={300} width={300} className="spinning-bubbles"/>
+};
+export default Loading;
