fc156a7fdf4e050c1d76cd7deefee384b4457608
Ibrahim Albrahim <hemo7f12@gmail.com>
2022-02-18 21:35:25 +0300

    fix style for phone

--- frontend/src/routes/components/Loading.js
+++ frontend/src/routes/components/Loading.js
@@ -1,6 +1,7 @@
 import ReactLoading from 'react-loading';
+import '../../scss/Loading.scss'
 
 const Loading = () => {
-    return  <ReactLoading type="spinningBubbles" color="#ffffff" height={300} width={300} className="spinning-bubbles"/>
+    return  <ReactLoading type="spinningBubbles" color="#ffffff" height={200} width={200} className="spinning-bubbles"/>
 };
 export default Loading;
